76a0c4bfff29de124ac138737974fc9752b5900b
Francis Coteur <fcoteur@hotmail.com>
2018-11-01 07:37:49 +0100

    data fecthed through promise

--- index.js
+++ index.js
@@ -1,108 +1,100 @@
-let educationUrl = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json'
-let countiesUrl = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
+let data = new Map();
+let us = {};
+data.title = "Education rate (%)";
 
-function loadData(src) {
+Promise.all([loadTopo(), loadData()])
+   .then(
+    responses => renderD3()
+)
+function loadData() {
     return new Promise(function(resolve, reject){
-        let data = new Map();
-        data.title = "Education rate (%)";
-        req = new XMLHttpRequest();
-        req.open("GET",src,true);
-        req.send();
-        req.onload = () => {
-        json=JSON.parse(req.responseText);
-        data = json.map(d => {
-            data.set([d.fips, d.bachelorsOrHigher])
-        });
-        resolve(data);
+        req1 = new XMLHttpRequest();
+        req1.open("GET",'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',true);
+        req1.send();
+        req1.onload = () => {
+            let json = JSON.parse(req1.responseText);
+            let map = json.map(d => {
+                data.set(d.fips, d.bachelorsOrHigher)
+            });
+            resolve('loadData done!');
         }
     })
 }
-let promise = loadData(educationUrl);
+function loadTopo() {
+    return new Promise(function(resolve, reject){
+        req2 = new XMLHttpRequest();
+        req2.open("GET",'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json',true);
+        req2.send();
+        req2.onload = () => {
+            let json = JSON.parse(req2.responseText);
+            us = Object.assign({},json);
+            resolve('loadTopo done!');
+        }  
+    })
+}
+function renderD3() {
+    console.log('starting!')
+    const width = 960;
+    const height = 600;
+    const path = d3.geoPath();
 
-promise.then(
-    data => console.log(data)
-    
-)
+    const color = d3.scaleQuantize()
+        .domain([1, 10])
+        .range(d3.schemeBlues[9])
 
-/*
-  function loadTopo(src) {
-    let topo = {};
-    req = new XMLHttpRequest();
-    req.open("GET",src,true);
-    req.send();
-    req.onload = () => {
-        json=JSON.parse(req.responseText);
-        topo = json.map(d => {
-            topo.push(d)
-        })
-        return topo;
-    }  
-  }
-  
-  const topo = await getTopo();
+    const format = d3.format("")
 
+    const x = d3.scaleLinear()
+        .domain(d3.extent(color.domain()))
+        .rangeRound([600, 860]);
 
-const width = 960;
-const height = 600;
-const path = d3.geoPath();
+    const svg = d3.select('#map').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style("width", "100%")
+        .style("height", "auto");
 
-const color = d3.scaleQuantize()
-    .domain([1, 10])
-    .range(d3.schemeBlues[9])
+    const g = svg.append("g")
+        .attr("transform", "translate(0,40)");
 
-const format = d3.format("")
+    g.selectAll("rect")
+    .data(color.range().map(d => color.invertExtent(d)))
+    .enter().append("rect")
+        .attr("height", 8)
+        .attr("x", d => x(d[0]))
+        .attr("width", d => x(d[1]) - x(d[0]))
+        .attr("fill", d => color(d[0]));
 
-const x = d3.scaleLinear()
-    .domain(d3.extent(color.domain()))
-    .rangeRound([600, 860]);
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", x.range()[0])
+        .attr("y", -6)
+        .attr("fill", "#000")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text(data.title);
 
-const svg = d3.select('#map').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style("width", "100%")
-    .style("height", "auto");
+    g.call(d3.axisBottom(x)
+        .tickSize(13)
+        .tickFormat(format)
+        .tickValues(color.range().slice(1).map(d => color.invertExtent(d)[0])))
+    .select(".domain")
+        .remove();
 
-const g = svg.append("g")
-    .attr("transform", "translate(0,40)");
+    svg.append("g")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+        .attr("fill", d => color(data.get(d.id)))
+        .attr("d", path)
+    .append("title")
+        .text(d => format(data.get(d.id)));
 
-g.selectAll("rect")
-.data(color.range().map(d => color.invertExtent(d)))
-.enter().append("rect")
-    .attr("height", 8)
-    .attr("x", d => x(d[0]))
-    .attr("width", d => x(d[1]) - x(d[0]))
-    .attr("fill", d => color(d[0]));
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", path);
 
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text(data.title);
-
-g.call(d3.axisBottom(x)
-    .tickSize(13)
-    .tickFormat(format)
-    .tickValues(color.range().slice(1).map(d => color.invertExtent(d)[0])))
-.select(".domain")
-    .remove();
-
-svg.append("g")
-.selectAll("path")
-.data(topojson.feature(us, us.objects.counties).features)
-.enter().append("path")
-    .attr("fill", d => color(data.get(d.id)))
-    .attr("d", path)
-.append("title")
-    .text(d => format(data.get(d.id)));
-
-svg.append("path")
-    .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-    .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("stroke-linejoin", "round")
-    .attr("d", path);
-
-*/
+}
